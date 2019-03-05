f85b3e4460b54cdb1ff66fbc65514be265f01eb2
Fallauthy <k.borkowski0@gmail.com>
2019-03-05 16:56:59 +0100

    Added new project

---
+++ event-form/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Header.css'
+
+const header = () => (
+    <div className={styles.EventWrapper}>
+        <h1>New Event</h1>
+    </div>
+)
+
+export default header;
